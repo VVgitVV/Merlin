06e702269b6ac0dcc6bf616f159dc1814d062a68
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-28 13:00:10 +0000

    added validations for user, client, project models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ApplicationRecord
   belongs_to :user
+  has_many :projects
+
+  validates :name, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :client
+  has_many :timesheets
+
+  validates :name, :hourly_rate, :completed, presence: true
 end
