6495a1e10a9c2eb673f6af09d8edbed34dff3f23
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-29 19:00:26 +0000

    started timestamps

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -1,3 +1,22 @@
 class TimestampsController < ApplicationController
   # make methods here like calculate_date
+  def index
+    @client = Client.find(params[:client_id])
+    @project = Project.find(params[:project_id])
+    @timesheet = Timesheet.find(params[:timesheet_id])
+    @timestamp = Timestamp.new
+  end
+
+  def create
+    @timesheet = Timesheet.find(params[:timesheet_id])
+    @timestamp = Timestamp.new
+    @timestamp.timesheet = @timesheet
+    @timestamp.start_time = DateTime.now
+    render :new, status: :unprocessable_entity unless @timestamp.save
+    # if @timestamp.save
+    #   redirect_to client_project_timesheet_timestamps_path
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
 end
