77f4b1b0c8eb3ddd174fc37eb3a663aace8ffa1f
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-29 23:11:53 +0000

    added total method for timestamp

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,4 +1,12 @@
 class Timesheet < ApplicationRecord
   belongs_to :project
   has_many :timestamps
+
+  def total
+    count = 0
+    timestamps.each do |timestamp|
+      count += timestamp.subtotal unless timestamp.end_time.nil?
+    end
+    format('%.2f', count)
+  end
 end
