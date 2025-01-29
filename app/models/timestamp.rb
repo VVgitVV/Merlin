bc8b16f6eee36dc000ddd249cf4c2c84df0bd151
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-29 21:43:05 +0000

    more timestamps methods / starting timesheet table

--- app/models/timestamp.rb
+++ app/models/timestamp.rb
@@ -1,11 +1,15 @@
 class Timestamp < ApplicationRecord
   belongs_to :timesheet
 
-  def calculate_date
+  def date
     end_time.strftime('%m/%d/%Y')
   end
 
-  def calculate_hours
+  def hours
     ((end_time.to_f - start_time.to_f) / 3600).round(2)
+  end
+
+  def subtotal
+    timesheet.project.hourly_rate * hours
   end
 end
