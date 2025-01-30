af65d98c88ac6de675d70b011c5c85b0600ccae1
Kit Trowbridge <kittrow.dev@gmail.com>
2025-01-30 17:01:10 +0000

    refactored timestamps controller, still need to work on displaying update form

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -1,15 +1,11 @@
 class TimestampsController < ApplicationController
+  before_action :set_chained_instances
+
   def index
-    @client = Client.find(params[:client_id])
-    @project = Project.find(params[:project_id])
-    @timesheet = Timesheet.find(params[:timesheet_id])
     @timestamp = Timestamp.new
   end
 
   def create
-    @client = Client.find(params[:client_id])
-    @project = Project.find(params[:project_id])
-    @timesheet = Timesheet.find(params[:timesheet_id])
     @timestamp = Timestamp.new
     @timestamp.timesheet = @timesheet
     @timestamp.start_time = DateTime.now
@@ -21,11 +17,30 @@ class TimestampsController < ApplicationController
   end
 
   def stop
-    @client = Client.find(params[:client_id])
-    @project = Project.find(params[:project_id])
-    @timesheet = Timesheet.find(params[:timesheet_id])
     @timestamp = Timestamp.find(params[:id])
     @timestamp.update(end_time: DateTime.now)
     redirect_to client_project_timesheet_timestamps_path(@client, @project, @timesheet)
+  end
+
+  def edit
+    @timestamp = Timestamp.find(params[:id])
+  end
+
+  def update
+    @timestamp = Timestamp.find(params[:id])
+    @timestamp.update(timestamp_params)
+    redirect_to client_project_timesheet_timestamps_path(@client, @project, @timesheet)
+  end
+
+  private
+
+  def set_chained_instances
+    @client = Client.find(params[:client_id])
+    @project = Project.find(params[:project_id])
+    @timesheet = Timesheet.find(params[:timesheet_id])
+  end
+
+  def timestamp_params
+    params.require(:timestamp).permit(:task_description)
   end
 end
