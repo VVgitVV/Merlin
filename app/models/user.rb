a0b4fe2cd354249297734c5e4b7c74da60ea8d65
Alison Harthill <arthilla@outlook.com>
2025-02-03 10:17:24 +0000

    chatbot setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :clients
+  has_many :questions
 
   validates :first_name, :last_name, :username, presence: true
 end
