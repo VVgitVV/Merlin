5fd4e8655db5829e0b554351fb8d184fac25d5da
Kit Trowbridge <kittrow.dev@gmail.com>
2025-02-03 16:57:44 +0000

    setting up pdf generation

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -2,6 +2,7 @@ require 'date'
 
 class TimesheetsController < ApplicationController
   before_action :set_project, only: :create
+  skip_before_action :authenticate_user!, only: %i[show generate_pdf]
 
   def index
     @timesheets = Timesheet.all.order(:created_at)
@@ -24,6 +25,8 @@ class TimesheetsController < ApplicationController
   end
 
   def generate_pdf
+    @timesheet = Timesheet.find(params[:id])
+    use_docraptor(@timesheet)
   end
 
   private
@@ -31,5 +34,32 @@ class TimesheetsController < ApplicationController
   def set_project
     @client = Client.find(params[:client_id])
     @project = @client.projects.find(params[:project_id])
+  end
+
+  def use_docraptor(timesheet)
+    require "docraptor"
+
+    DocRaptor.configure do |config|
+      config.username = "YOUR_API_KEY_HERE" # this key works in test mode!
+    end
+
+    docraptor = DocRaptor::DocApi.new
+
+    begin
+      response = docraptor.create_doc(
+        test: true, # test documents are free but watermarked
+        document_type: "pdf",
+        document_url: "https://www.lancelot.quest/clients/#{timesheet.project.client.id}/projects/#{timesheet.project.id}/timesheets/#{timesheet.id}"
+      )
+
+      # create_doc() returns a binary string
+      File.write("Timesheet_#{timesheet.id}.pdf", response, mode: "wb")
+      puts "Successfully created docraptor-hello.pdf!"
+    rescue DocRaptor::ApiError => error
+      puts "#{error.class}: #{error.message}"
+      puts error.code
+      puts error.response_body
+      puts error.backtrace[0..3].join("\n")
+    end
   end
 end
