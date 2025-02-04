6e68adc9613ef9a692a477dc2de12a5edc81416b
Alison Harthill <arthilla@outlook.com>
2025-02-04 11:39:17 +0000

    gem/schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_02_161347) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_02_03_170509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_02_161347) do
     t.string "name"
     t.text "aim"
     t.float "hourly_rate"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.bigint "client_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
