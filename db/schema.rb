f785ba987aa710115d3bab5841f1a6d873cb82b3
Alison Harthill <arthilla@outlook.com>
2025-02-06 15:00:53 +0000

    popup image and placing

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_02_03_170509) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_05_125843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
